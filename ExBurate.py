c60c5d86411db650ea740a03de07d7df2cbdbfd1
Eto <69520379+breadx333@users.noreply.github.com>
2020-09-06 17:20:29 +0500

    Update ExBurate.py

--- ExBurate.py
+++ ExBurate.py
@@ -24,29 +24,35 @@ class TestWindow(wx.Frame):
 
         self.panel = wx.Panel(self, wx.ID_ANY)
 
+        #self.status_bar = self.CreateStatusBar(style=0)
+
         VSizer = wx.BoxSizer(wx.VERTICAL)
+        HSizer = wx.BoxSizer(wx.HORIZONTAL)
+        H2Sizer = wx.BoxSizer(wx.HORIZONTAL)
 
         static_line1 = wx.StaticLine(self.panel)
-        VSizer.Add(static_line1, 0, wx.EXPAND|wx.ALL, 5)
+        HSizer.Add(static_line1, 1, wx.ALIGN_CENTER|wx.ALL, 5)
+        VSizer.Add(HSizer, 1, wx.EXPAND, 5)
 
         #self.png = wx.StaticBitmap(self, -1, wx.Bitmap("Background.png", wx.BITMAP_TYPE_ANY))
         #VSizer.Add(self.png, 0, wx.ALL, 5)
 
-        self.NameText = wx.StaticText(self.panel, wx.ID_ANY, label="ExBurate ver.0.1")
-        VSizer.Add(self.NameText, 0, wx.CENTER|wx.ALL, 5)
+        self.NameText = wx.StaticText(self.panel, wx.ID_ANY, label="Welcome to ExBurate ver.0.2!")
+        VSizer.Add(self.NameText, 1, wx.CENTER|wx.ALL, 5)
 
         self.AddBox = wx.TextCtrl(self.panel, wx.ID_ANY, style=wx.TE_PROCESS_ENTER|wx.TE_CENTER)
-        VSizer.Add(self.AddBox, 0, wx.CENTER|wx.ALL, 5)
+        VSizer.Add(self.AddBox, 1, wx.CENTER|wx.ALL, 5)
 
         self.chText = wx.StaticText(self.panel, wx.ID_ANY, label="Click Random")
         self.chText.SetFont(wx.Font(16, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, wx.EmptyString))
-        VSizer.Add(self.chText, 0, wx.CENTER|wx.ALL, 5)
+        VSizer.Add(self.chText, 1, wx.CENTER|wx.ALL, 5)
         
         self.chBtn = wx.Button(self.panel, wx.ID_ANY, label="Random!")
-        VSizer.Add(self.chBtn, 0, wx.CENTER|wx.ALL, 5)
+        VSizer.Add(self.chBtn, 1, wx.CENTER|wx.ALL, 5)
 
         static_line1 = wx.StaticLine(self.panel)
-        VSizer.Add(static_line1, 0, wx.EXPAND|wx.ALL, 5)
+        H2Sizer.Add(static_line1, 1, wx.ALIGN_CENTER|wx.ALL, 5)
+        VSizer.Add(H2Sizer, 1, wx.EXPAND, 5)
 
         self.AddBox.Bind(wx.EVT_TEXT_ENTER, self.AddWordEnter)
         self.chBtn.Bind(wx.EVT_BUTTON, self.chBtnClick)
